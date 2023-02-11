e95713580576fe32a728ddcf06ff1fc5aafed71f
Shrey Pokhriyal <89126760+Shrey-Pokhriyal@users.noreply.github.com>
2023-02-12 00:59:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin:'*',
     credentials: true,
   })
 );
